4001fb13b27af9c8b3b2a5bd20915514ca63dc65
karthik13vk <vijayakarthik13@gmail.com>
2025-04-23 17:33:56 +0530

    first commit

---
+++ src/component/banner/Banner.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Banner.scss'
+import bannerimg from './../img/banner-img1.png';
+import bannerimgres from './../img/banner-img.png';
+const Banner = () => {
+    return (
+        <section className='Banner-section'>
+            <div className="container">
+                <div className="row justify-content-between">
+                    <div className="col-lg-6 col-md-12 order-md-2 order-2 order-lg-1"  data-aos="fade-up">
+                        <div className="banner-content">
+                            <h1>nRWA Token</h1>
+                            <h6> Powering the Future of Real-World Asset Tokenization</h6>
+                            <p>
+                                The NanoEx RWA ecosystem, powered by nRWA Token, revolutionizes how real-world assets are tokenized, traded, and managed on the blockchain. Through a transparent tokenization framework, we enable fractional ownership, enhanced liquidity, and secure investments in traditionally illiquid assets like real estate, fine art, and infrastructure. Join the Future of Asset Tokenization Today!
+                            </p>
+                            <div className='btn-area d-flex gap-2 mt-4'>
+                                <a href='javascript:void(0)' className='main-btn'>Whitepaper</a>
+                                <a href='javascript:void(0)' className='gray-btn'>How to Buy</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-5 col-md-12 order-md-1 order-1 order-lg-2"  data-aos="fade-up">
+                        <div className="banner-right-bg">
+                            <img src={bannerimg} alt="bannerimg" loading="lazy" className='d-none d-lg-block' />
+                            <img src={bannerimgres} alt="bannerimg" loading="lazy" className='d-block d-lg-none' />
+                        </div>
+                    </div>
+                </div>
+            </div></section>
+    )
+}
+
+export default Banner
---
+++ src/component/ecosystem/Ecosystem.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import ecosystemimg from './../img/ecosystem.png';
+import './Ecosystem.scss'
+const Ecosystem = () => {
+    return (
+        <section className='ecosystem-section'>
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-6 col-md-12">
+                        <div className="ecosystem-img" data-aos="flip-left" data-aos-duration="1500">
+                            <img src={ecosystemimg} alt="img"  loading="lazy"/>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-12 " data-aos="fade-up" data-aos-duration="2000">
+                        <div className="ecosystem-content">
+                            <div className="heading-section">
+                                <h2 className='heading-title'>NanoEx Ecosystem</h2>
+                                <p>Our ecosystem supports the entire lifecycle of digital asset trading and investment,
+                                    offering seamless transactions, secure payments, automation, and rewarding opportunities.
+                                </p>
+                            </div>
+                            <div className='inner-content'>
+                                <div className='inner-line'>
+                                    <h3>RWA Marketplace </h3>
+                                    <p>
+                                        A blockchain-powered platform for tokenizing real-world assets, enabling fractional ownership, enhanced liquidity, and secure global asset trading.
+                                    </p>
+                                </div>
+                                <div className='inner-line'>
+                                    <h3>Affiliate Program </h3>
+                                    <p>
+                                        Earn rewards by referring users, promoting the platform, and growing the ecosystem through strategic partnerships and community-driven incentives.
+                                    </p>
+                                </div>
+                                <div className='inner-line'>
+                                    <h3>Fiat-to-Crypto (P2P) </h3>
+                                    <p>
+                                        Convert fiat to crypto securely through a decentralized peer-to-peer network, enabling fast and transparent transactions without intermediaries.
+                                    </p>
+                                </div>
+                                <div className='inner-line'>
+                                    <h3>Nano Pay (Bank Transfers) </h3>
+                                    <p>
+                                        Facilitate seamless and instant bank transfers for crypto-related transactions, bridging traditional finance with blockchain-powered payments.
+                                    </p>
+                                </div>
+                                <div className='inner-line'>
+                                    <h3>Fiat-to-Crypto (P2P) </h3>
+                                    <p>
+                                        Convert fiat to crypto securely through a decentralized peer-to-peer network, enabling fast and transparent transactions without intermediaries.
+                                    </p>
+                                </div>
+                                <div className='inner-line'>
+                                    <h3>Nano Pay (Bank Transfers) </h3>
+                                    <p>
+                                        Facilitate seamless and instant bank transfers for crypto-related transactions, bridging traditional finance with blockchain-powered payments.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Ecosystem
